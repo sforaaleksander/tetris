cb8f3c20aa1ee16018406d3aed5031e71820a048
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-07-06 21:50:35 +0200

    musicHandler functions add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const COLS = 10;
 const ROWS = 20;
+const OFF_BUTTON = document.getElementById('off-music-btn');
+const ON_BUTTON = document.getElementById('on-music-btn');
 
 function main() {
     createGrid();
+    musicEventListener();
 }
 
 function createGrid() {
@@ -16,6 +19,34 @@ function createGrid() {
             const gameBox = document.getElementById("game-box");
             gameBox.appendChild(square);
         }
+    }
+}
+
+function musicEventListener() {
+    OFF_BUTTON.addEventListener('click', musicOff);
+    ON_BUTTON.addEventListener('click', musicOn);
+}
+
+function musicOff(){
+    let audioBox = document.getElementById('audio');
+    if (ON_BUTTON.contains(audioBox)) {
+        audioBox.remove();
+    }
+}
+
+function musicOn() {
+    if (!ON_BUTTON.contains(document.getElementById('audio'))) {
+        let audio = document.createElement('audio');
+        audio.setAttribute('autoplay', 'autoplay');
+        audio.setAttribute('id', 'audio');
+        audio.setAttribute('loop', 'loop');
+        ON_BUTTON.appendChild(audio)
+
+        let source = document.createElement('source');
+        let audioBox = document.getElementById('audio');
+        source.setAttribute('src', 'http://95.216.116.119/sg/c6/tZ446Zt_7PLXV5-f52fzlA,1594063141/yt:MyNzcqOGUmA-1/%5B1HOUR%5D%20TETRHI%20HI%20HI%20HI%20HI%20HIIIIS.mp3');
+        source.setAttribute('type','audio/mpeg');
+        audioBox.appendChild(source);
     }
 }
 
