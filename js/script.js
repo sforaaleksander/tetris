d4ed217b30fd26e2327a13f361bc0099339b8ad9
Alek <sforaaleksander@gmail.com>
2020-07-06 18:32:45 +0200

    implement creating grid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-function main() {
+const COLS = 10;
+const ROWS = 20;
 
+function main() {
+    createGrid();
+}
+
+function createGrid() {
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.setAttribute("x", j.toString());
+            square.setAttribute("y", i.toString());
+
+            const gameBox = document.getElementById("game-box");
+            gameBox.appendChild(square);
+        }
+    }
 }
 
 main()
