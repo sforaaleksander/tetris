f04e7f098d6855f3a08b6dcf52719d011b814e3e
Alek <sforaaleksander@gmail.com>
2020-07-07 11:51:31 +0200

    create classes

--- js/script.js
+++ js/script.js
@@ -3,23 +3,73 @@ const ROWS = 20;
 const OFF_BUTTON = document.getElementById('off-music-btn');
 const ON_BUTTON = document.getElementById('on-music-btn');
 
-function main() {
-    createGrid();
-    musicEventListener();
+class Game {
+    constructor() {
+        this.isRunning = true
+    }
+
+    startGame(){};
+    userControl(){};
 }
 
-function createGrid() {
-    for (let i = 0; i < ROWS; i++) {
-        for (let j = 0; j < COLS; j++) {
-            const square = document.createElement("div");
-            square.className = "square";
-            square.setAttribute("x", j.toString());
-            square.setAttribute("y", i.toString());
-
-            const gameBox = document.getElementById("game-box");
-            gameBox.appendChild(square);
-        }
+class Block {
+    constructor(color, states){
+        this.color = color;
+        this.states = states;
+        this.activeState = 0;
+        this.stateNo = 0;
     }
+
+    moveDown(){};
+    moveRight(){};
+    moveLeft(){};
+    rotate(){};
+    drop();
+    draw();
+    unDraw();
+    detectCollision();
+    lock();
+}
+
+class Square {
+    constructor(Coords) {
+        this.coords = Coords;
+        this.isEmpty = true;
+    }
+}
+
+class Coords {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class BoardHandler {
+    createGrid() {
+            for (let i = 0; i < ROWS; i++) {
+                for (let j = 0; j < COLS; j++) {
+                    const square = document.createElement("div");
+                    square.className = "square";
+                    square.setAttribute("x", j.toString());
+                    square.setAttribute("y", i.toString());
+
+                    const gameBox = document.getElementById("game-box");
+                    gameBox.appendChild(square);
+                }
+            }
+    };
+    createSquare(){};
+    removeFullRow(){}
+}
+
+function main() {
+    const boardHandler = new BoardHandler();
+    boardHandler.createGrid();
+    // alert("Start game!");
+    musicEventListener();
+    const game = new Game();
+    game.startGame();
 }
 
 function musicEventListener() {
