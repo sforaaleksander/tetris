213dc6762b86959ef87a7537a2d4c12f75d59e86
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-07-07 20:11:18 +0200

    moving block and rotating fix

--- js/block.js
+++ js/block.js
@@ -1,10 +1,10 @@
 export class Block {
     constructor(color, states){
-        this.x = 3;
-        // this.y = -this.states[0][0].length;
-        this.y = 0;
+        // this.y = 0;
         this.color = color;
         this.states = states;
+        this.x = 3;
+        this.y = -this.states[0][0].length;
         this.stateNo = 0;
         this.activeState = this.states[this.stateNo];
     }
@@ -33,18 +33,27 @@ export class Block {
     };
 
     rotate(){
-        console.log('rotate')
+        this.unDraw();
+        this.stateNo = (this.stateNo + 1)%this.states.length;
+        this.activeState = this.states[this.stateNo];
+        this.draw();
     };
 
     drop(){
         this.y++;
     };
+
     draw() {
+        console.log(this)
         for (let i = 0; i < this.activeState.length; i++) {
             for (let j = 0; j < this.activeState.length; j++) {
                 if (this.activeState[i][j]) {
-                    let square = document.querySelector(`[x="${i}"][y="${j}"]`);
-                    square.classList.add(this.color);
+                    try {
+                        let square = document.querySelector(`[x="${this.x+i}"][y="${this.y+j}"]`);
+                        square.classList.add(this.color);
+                    } catch (e) {
+
+                    }
                 }
             }
         }
@@ -54,8 +63,12 @@ export class Block {
         for (let i = 0; i < this.activeState.length; i++) {
             for (let j = 0; j < this.activeState.length; j++) {
                 if (this.activeState[i][j]) {
-                    let square = document.querySelector(`[x="${i}"][y="${j}"]`);
-                    square.classList.remove(this.color);
+                    try {
+                        let square = document.querySelector(`[x="${this.x+i}"][y="${this.y+j}"]`);
+                        square.classList.remove(this.color);
+                    }catch (e) {
+
+                    }
                 }
             }
         }
--- js/controlHandler.js
+++ js/controlHandler.js
@@ -3,15 +3,17 @@ export class ControlHandler {
     constructor(block) {
         this.block = block;
         this.arrows = {
-            37: this.block.moveLeft,
-            40: this.block.moveDown,
-            38: this.block.rotate,
-            39: this.block.moveRight
+            37: () => {this.block.moveLeft()},
+            40: () => {this.block.moveDown()},
+            38: () => {this.block.rotate()},
+            39: () => {this.block.moveRight()}
         };
     }
 
     addControlsListener() {
-        document.addEventListener('keydown', (e) => {this.arrows[e.keyCode]();
+        document.addEventListener('keydown', (e) => {
+            console.log(this)
+            this.arrows[e.keyCode]();
         });
     }
 }
