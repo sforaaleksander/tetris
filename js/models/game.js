54157c0609054b880871f33624b38d30f631edbd
Alek <sforaaleksander@gmail.com>
2020-07-08 19:26:28 +0200

    move game logic to game.js

--- js/models/game.js
+++ js/models/game.js
@@ -1,9 +1,47 @@
+import * as handlers from "../handlers/handlersBundle.js";
+import {BlockFactory} from "./blockFactory.js";
+
+
+let block = BlockFactory.prototype.getRandomBlock();
+let nextStatesAndColor = BlockFactory.prototype.getRandomStateAndColor();
+let controlHandler = new handlers.ControlHandler(block);
+let loop = undefined;
+
 export class Game {
     constructor() {
         this.isRunning = true
         this.level = 1;
         this.points = 0;
+        this.initialize();
+        this.startGame();
     }
 
-    startGame(){};
+    initialize(){
+        const boardHandler = new handlers.BoardHandler();
+        boardHandler.createGrid();
+        alert("Start game!");
+        handlers.MusicHandler.prototype.musicEventListener()
+        controlHandler.addControlsListener();
+    }
+
+    mainLoop(){
+        let canMove = block.moveDown();
+        if (block.getIsLocked()) {
+            if (!canMove) {
+                clearInterval(loop);
+                alert("GameOverXD");
+                this.isRunning = false;
+                return;
+            }
+            nextStatesAndColor = BlockFactory.prototype.getRandomStateAndColor();
+            const color = nextStatesAndColor[0];
+            const states = nextStatesAndColor[1];
+            block.resetBlock(states, color);
+        }
+    }
+
+
+    startGame(){
+        loop = setInterval(this.mainLoop,500);
+    };
 }
--- js/script.js
+++ js/script.js
@@ -1,47 +1,11 @@
-import * as gameJS from "./models/game.js";
-import {BlockFactory} from "./models/blockFactory.js";
-import * as handlers from './handlers/handlersBundle.js';
+import {Game} from "./models/game.js";
 
-let block;
-let nextStatesAndColor;
-let controlHandler;
+
 let game;
-let loop;
 
 (() => {
-    initialize();
-    loop = setInterval(mainLoop,500);
+    game = new Game()
+    // game.initialize();
+    // game.startGame();
 })();
-
-function initialize(){
-    const boardHandler = new handlers.BoardHandler();
-    boardHandler.createGrid();
-    alert("Start game!");
-    game = new gameJS.Game();
-    handlers.MusicHandler.prototype.musicEventListener()
-    game.startGame();
-
-    block = BlockFactory.prototype.getRandomBlock();
-    nextStatesAndColor = BlockFactory.prototype.getRandomStateAndColor();
-    controlHandler = new handlers.ControlHandler(block);
-    controlHandler.addControlsListener();
-}
-
-
-function mainLoop(){
-    let canMove = block.moveDown();
-    if (block.isLocked) {
-        if (!canMove) {
-            clearInterval(loop);
-            alert("GameOverXD");
-            game.isRunning = false;
-            return;
-        }
-        nextStatesAndColor = BlockFactory.prototype.getRandomStateAndColor();
-        const color = nextStatesAndColor[0];
-        const states = nextStatesAndColor[1];
-        block.resetBlock(states, color);
-    }
-}
-
 
