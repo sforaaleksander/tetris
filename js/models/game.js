cbdb8b5e7e5b7b7b4993040c6a4e2da583d37fa7
Alek <sforaaleksander@gmail.com>
2020-07-09 17:19:10 +0200

    implement gaining points and levels

--- js/models/game.js
+++ js/models/game.js
@@ -8,25 +8,27 @@ let controlHandler = new handlers.ControlHandler(block);
 let loop = undefined;
 let boardHandler;
 
+
 export class Game {
     constructor() {
-        this.isRunning = true
         this.level = 1;
         this.points = 0;
+        this.isRunning = true;
         this.initialize();
         this.startGame();
     }
 
-    initialize(){
+    initialize() {
         boardHandler = new handlers.BoardHandler();
         boardHandler.createGrid();
         alert("Start game!");
         handlers.MusicHandler.prototype.musicEventListener()
         controlHandler.addControlsListener();
-        boardHandler.setNextBlock(nextStatesAndColor[0][0],nextStatesAndColor[1]);
+        boardHandler.setNextBlock(nextStatesAndColor[0][0], nextStatesAndColor[1]);
+        this.updateScore();
     }
 
-    mainLoop(){
+    mainLoop() {
         let canMove = block.drop();
         if (block.isLocked) {
             if (!canMove) {
@@ -35,6 +37,16 @@ export class Game {
                 this.isRunning = false;
                 return;
             }
+            if (this.isNextLevel()) {
+                console.log("NEXT LEVEL");
+                this.level++;
+                clearInterval(loop);
+                loop = setInterval(this.mainLoop.bind(this), 600 - (10 * this.level));
+            }
+            let clearedRows = handlers.BoardHandler.prototype.checkFullRow();
+            this.givePoints(clearedRows);
+            console.log(this.points);
+            this.updateScore();
             let states = nextStatesAndColor[0];
             let color = nextStatesAndColor[1];
             block.resetBlock(states, color);
@@ -46,7 +58,27 @@ export class Game {
         }
     }
 
-    startGame(){
-        loop = setInterval(this.mainLoop,500);
-    };
+    startGame() {
+        loop = setInterval(this.mainLoop.bind(this), 600);
+    }
+
+    givePoints(clearedRows) {
+        let pointsAchieved = clearedRows * (this.level + this.level / 4) * 100;
+        if (clearedRows >= 4) {
+            pointsAchieved += 999;
+        }
+        console.log("GIVE POINTS")
+        this.points += pointsAchieved;
+    }
+
+    isNextLevel() {
+         return (this.points - this.level * 1000) > 1000;
+    }
+
+    updateScore() {
+        const score = document.getElementById("game-score");
+        const level = document.getElementById("game-level");
+        score.innerHTML = this.points;
+        level.innerHTML = this.level;
+    }
 }
