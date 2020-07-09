c4d0ffa52c8d88e25b8a349cea3f57805a6aea72
Alek <sforaaleksander@gmail.com>
2020-07-09 17:21:37 +0200

    debug initial score and level display

--- js/models/game.js
+++ js/models/game.js
@@ -25,7 +25,6 @@ export class Game {
         handlers.MusicHandler.prototype.musicEventListener()
         controlHandler.addControlsListener();
         boardHandler.setNextBlock(nextStatesAndColor[0][0], nextStatesAndColor[1]);
-        this.updateScore();
     }
 
     mainLoop() {
